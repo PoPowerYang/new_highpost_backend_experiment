ed3ae11ddb5312c3e674b120fba580dc1eb40cd5
PoPowerYang <yangsunsunshine@gmail.com>
2019-08-06 14:05:42 -0400

    all then blocks are now replaced with async await keywords, which improves the readability of code.

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,65 +5,87 @@ const unique = require('uniqid');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        const error = new Error('Validation failed.');
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
-    }
-    const email = req.body.email;
-    const name = req.body.name;
-    const password = req.body.password;
-    const age = req.body.age;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const address = req.body.address;
-    const phone = req.body.phone;
-    bcrypt
-        .hash(password, 12)
-        .then(hashedPw => {
-            return User.create({
-                id: unique(),
-                email: email,
-                name: name,
-                password: hashedPw,
-                age: age,
-                firstName: firstName,
-                lastName: lastName,
-                address: address,
-                phone: phone,
-            });
-        })
-        .then(result => {
-            res.status(201).json({ message: 'User created!', userId: result.id });
-        })
-        .catch(err => {
-            if(!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
+exports.signup = async (req, res, next) => {
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            const error = new Error('Validation failed.');
+            error.statusCode = 422;
+            error.data = errors.array();
+            throw error;
+        }
+        const email = req.body.email;
+        const name = req.body.name;
+        const password = req.body.password;
+        const age = req.body.age;
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const address = req.body.address;
+        const phone = req.body.phone;
+        
+        const hashedPw = await bcrypt.hash(password, 12);
+        const result = await User.create({
+            id: unique(),
+            email: email,
+            name: name,
+            password: hashedPw,
+            age: age,
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            phone: phone,
         });
+        res.status(201).json({ message: 'User created!', userId: result.id });
+    } catch (error) {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+    // bcrypt
+    //     .hash(password, 12)
+    //     .then(hashedPw => {
+    //         return User.create({
+    //             id: unique(),
+    //             email: email,
+    //             name: name,
+    //             password: hashedPw,
+    //             age: age,
+    //             firstName: firstName,
+    //             lastName: lastName,
+    //             address: address,
+    //             phone: phone,
+    //         });
+    //     })
+    //     .then(result => {
+    //         res.status(201).json({ message: 'User created!', userId: result.id });
+    //     })
+    //     .catch(err => {
+    //         if(!err.statusCode) {
+    //             err.statusCode = 500;
+    //         }
+    //         next(err);
+    //     });
 };
 
-exports.login = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let loadedUser;
-    User.findAll({
-        where: {
-            email: email,
-        }
-    }).then(user => {
-        if(user[0]==null) {
+exports.login = async (req, res, next) => {
+
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+        let loadedUser;
+
+        const user = await User.findAll({
+            where: {
+                email: email,
+            }
+        });
+        if(user[0] == null) {
             const error = new Error('A user with this email could not be found.');
             error.statusCode = 401;
             throw error;
         }
-        loadedUser = user[0];
-        return bcrypt.compare(password, loadedUser.password);
-    }) .then(isEqual => {
+        const isEqual = await bcrypt.compare(password, user[0].password);
         if(!isEqual) {
             const error = new Error('Wrong password!');
             error.statusCode = 401;
@@ -78,11 +100,44 @@ exports.login = (req, res, next) => {
             { expiresIn: '1h'}
         );
         res.status(200).json({ token: token, userId: loadedUser.id });
-    })
-    .catch(err => {
+    } catch (error) {
         if(!err.statusCode) {
             err.statusCode = 500;
         }
         next(err);
-    });
+    }
+    // User.findAll({
+    //     where: {
+    //         email: email,
+    //     }
+    // }).then(user => {
+    //     if(user[0]==null) {
+    //         const error = new Error('A user with this email could not be found.');
+    //         error.statusCode = 401;
+    //         throw error;
+    //     }
+    //     loadedUser = user[0];
+    //     return bcrypt.compare(password, loadedUser.password);
+    // }) .then(isEqual => {
+    //     if(!isEqual) {
+    //         const error = new Error('Wrong password!');
+    //         error.statusCode = 401;
+    //         throw error;
+    //     }
+    //     const token = jwt.sign(
+    //         {
+    //             email: loadedUser.email,
+    //             userId: loadedUser.id,
+    //         },
+    //         'mindweaverscrecetkey123!*#',
+    //         { expiresIn: '1h'}
+    //     );
+    //     res.status(200).json({ token: token, userId: loadedUser.id });
+    // })
+    // .catch(err => {
+    //     if(!err.statusCode) {
+    //         err.statusCode = 500;
+    //     }
+    //     next(err);
+    // });
 };
